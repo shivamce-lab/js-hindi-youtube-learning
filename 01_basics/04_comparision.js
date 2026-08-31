3d34ede8cca78bd7def8de41092efe5794b16d7f
shivamsscsingh1727-comp <shivamsscsingh1727@gmail.com>
2026-09-01 01:14:11 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2 >=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2 != 1);
+// console.log("2">1);// reason is 2 is greater. automatically 2 is converted to num by js
+// console.log("02">1);
+// console.log("1">2);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null == 0);// equality check don't convert null to a number. and conversion ><>=<= convert it to 0
+//avoid them. It can be comparision
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);=> false
+
+// console.log("2"===2);
+
+
+
+
