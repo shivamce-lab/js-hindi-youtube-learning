ae0f848f7acedc2d2657f46726dd1eada215eb24
shivamsscsingh1727-comp <shivamsscsingh1727@gmail.com>
2026-09-01 00:37:58 +0530

    conversion_string_number_boolean

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score= "Shivam"
+//const {score} = req.body
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+// value=> NaN answer, NaN Typeof
+// "33"=> 33, number
+// "33abc" => NaN, number
+//true=> 1; false=>0 NaN, number
+let isLoggedIn = "hitesh"
+let booleanLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanLoggedIn);
+
+// 1 => true, 0=> false
+//""=> false
+//"Hitesh"=> true
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
