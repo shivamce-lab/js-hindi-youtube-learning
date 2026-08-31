2bcbc8ae4920d2605de4f4f88614ce9463f58be5
shivamsscsingh1727-comp <shivamsscsingh1727@gmail.com>
2026-09-01 00:15:30 +0530

    Type of Data storing variables

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log(3+3);
+//code readability should be high.
+//alert(3+3)
+console.log("Hitesh");
+let name ="Hitesh";
+let age= 18;
+let isLoggedIn = false;
+// number => 2 to power 3
+//bigint
+//string =>""
+//boolean=> true/false
+//null =>standalone value
+//undefined =>Variable declared but not had value
+//symbol =>unique
+//above is primitive data type
+//
+console.log(typeof "Hitesh");
+console.log(typeof null);//type=>//object
+console.log(typeof undefined);//type=>undefined
