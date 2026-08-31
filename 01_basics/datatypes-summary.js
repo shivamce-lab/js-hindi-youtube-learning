6269ce7b62ffe25ce8602e02c8aeea69e87f191d
shivamsscsingh1727-comp <shivamsscsingh1727@gmail.com>
2026-09-01 01:40:37 +0530

    DataTypes Overview

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// data categoruisation 
+// #primitive : 7 categories : String, Number, Boolean, Null, undefined, Symbol, BigInt
+// Many times to make any object unique we wrap it in symobol. thats for now. BigInt
+// BigInt handles very big or scientific values
+
+// #non primitive
+// Reference (Non-Primitive)
+//Arrays, Objects, Functions
+//To tell type of variable: isnt needed in javascript
+const score= 10;
+//Dynamically typed Language
+// Type script, Data Type is mentioned there.
+// Decimal and Integers both considered inside numbers only.
+const id= Symbol('123');
+const anotherId=('123');
+let animal
+let birdpresent= null
+console.log(id == anotherId);
+const bigNumber = 34567893456783456789345678345678934567834567893456789n
+console.log(bigNumber);
+// array,objects, functions
+const heros = ["Shaktiman", "naagraj", "doga", "God"]
+let myObj= {
+    name: "Hitesh",
+    age: 22
+}
+// console.log(heros);
+// console.log(myObj);
+// definiton of funtion :function(){}
+const myFunction =function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof animal);
+console.log(typeof birdpresent);
+console.log(typeof id);
+console.log(typeof myFunction);
+
+
+
