b5addeec2edd9a4428ec944df2e766b7f60ac245
shivamsscsingh1727-comp <shivamsscsingh1727@gmail.com>
2026-09-01 01:01:01 +0530

    Conversion+Operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -20,6 +20,47 @@ let booleanLoggedIn = Boolean(isLoggedIn)
 //"Hitesh"=> true
 let someNumber=33
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*********Operations************
+let value= 3
+let negativeValue= -value
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1+str2
+//console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 +"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// reason is on ECMA SCRIPT 
+//console.log(3+4*5%3);
+// console.log(+true);
+// // console.log(-true);
+// console.log(+"");
+// console.log(-"Hitesh");//=> NaN
+
+let num1, num2, num3
+num1 = num2 = num3 =2+2
+//multiple assignments
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+++gameCounter;
+
+
+
+
 
